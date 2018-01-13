3f1c5219c973574a91f05f1a6fe9bf4ce4295144
voxqhuy <kohnewlife@gmail.com>
2018-01-13 00:24:24 -0500

    added reading and speaking

--- js.js
+++ js.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
-
-
 });
+
+function startTest() {
+	document.location.href = "reading.html";
+}
+
+function toSpeaking() {
+	document.location.href = "speaking.html";
+}
